1dd1b2f1d85b9389a47050abecc3d94b5644f198
developer-suhag <developersuhag@gmail.com>
2021-09-02 00:23:26 +0600

    show few data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // necessary variable 
 const searchField = document.getElementById('search-field');
+const bookContainer = document.getElementById('book-container');
 
 // load data
 const loadData = async () => {
@@ -11,13 +12,39 @@ const loadData = async () => {
     // clear search field 
     searchField.value = '';
     // call function
-    showBooks(data.docs);
+    bookContainer.innerHTML = '';
+    getBooks(data.docs);
 };
-// show books by search
-const showBooks = books => {
-    // console.log(books);
+// get all books by search
+const getBooks = books => {
     // loop through array
     books.forEach(book => {
-        console.log(book);
+        showBooks(book)
     });
+};
+
+// show books 
+
+const showBooks = book => {
+    console.log(book.title);
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('col');
+    bookCard.innerHTML = `
+        <div class="card h-100">
+            <img src="..." class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h4 class="card-title">${book.title}</h4>
+                <p>Author: ${book.author_name}</p>
+                <p class="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                </p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+    `;
+    bookContainer.appendChild(bookCard);
 }
