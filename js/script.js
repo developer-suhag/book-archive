53c0631ee097a0b3e19941eb58e68c769e28d9fa
developer-suhag <developersuhag@gmail.com>
2021-09-02 00:49:39 +0600

    error handle

--- js/script.js
+++ js/script.js
@@ -1,24 +1,49 @@
 // necessary variable 
 const searchField = document.getElementById('search-field');
 const bookContainer = document.getElementById('book-container');
+const bookFoundNumber = document.getElementById('book-number');
+const errorMessage = document.getElementById('error-message');
+
+// show error message 
+const showError = (message) => {
+    errorMessage.innerText = `${message}`;
+}
+
 
 // load data
 const loadData = async () => {
     const searchText = searchField.value;
-    // fetch data
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    // clear search field 
-    searchField.value = '';
-    // call function
-    bookContainer.innerHTML = '';
-    getBooks(data.docs);
+    // error handle for empty search field
+    if (searchField.value === '') {
+        showError('⚠️ Please search by book name to show results.');
+        bookFoundNumber.innerText = '';
+    } else {
+
+        // fetch data
+        const url = `http://openlibrary.org/search.json?q=${searchText}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        // clear search field 
+        searchField.value = '';
+        if (data.numFound === 0) {
+            showError('⚠️ No result found. PLease try again.');
+
+        } else {
+            // call function
+            bookContainer.innerHTML = '';
+            showError('');
+            getBooks(data.docs);
+        }
+    }
+
 };
 // get all books by search
 const getBooks = books => {
+    // console.log(books);
+    bookFoundNumber.innerText = `${books.length} Search result found.`;
     // loop through array
     books.forEach(book => {
+
         showBooks(book)
     });
 };
@@ -26,15 +51,17 @@ const getBooks = books => {
 // show books 
 
 const showBooks = book => {
-    console.log(book.title);
+    // console.log(book.title);
     const bookCard = document.createElement('div');
     bookCard.classList.add('col');
     bookCard.innerHTML = `
         <div class="card h-100">
             <img src="..." class="card-img-top" alt="..." />
             <div class="card-body">
-                <h4 class="card-title">${book.title}</h4>
-                <p>Author: ${book.author_name}</p>
+                <h3 class="card-title">${book.title}</h3>
+                <h5>Author: ${book.author_alternative_name}</h5>
+                <h5>Publishar: ${book.publisher}</h5>
+                <p>First publication: ${book.first_publish_year}</p>
                 <p class="card-text">
                     This is a wider card with supporting text below as a natural
                     lead-in to additional content. This content is a little bit
